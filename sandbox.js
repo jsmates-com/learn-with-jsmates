5b252066ae1a2697129406df4007160a9c3befd2
JS Mates <paramanantham@live.com>
2020-10-13 20:01:28 +0300

    init

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+const author = {};
+
+console.log(Object.keys(author));
+
+if (Object.keys(author).length > 0) {
+    console.log("Author object is not empty");
+} else {
+    console.log("Author object is empty");
+}
